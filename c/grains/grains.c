2934513a6d7a120f419a52bacdbf046564ce99dc
Luis Felipe Kunzler <kunzer.luis@gmail.com>
2024-03-23 23:58:38 -0300

    feat: grains

---
+++ c/grains/grains.c
@@ -0,0 +1,13 @@
+#include "grains.h"
+
+uint64_t square(uint8_t index)
+{
+    return ((uint64_t)(index > 0 && index < 65)) << (index-1);
+}
+
+uint64_t total(void)
+{
+    uint64_t max=0;
+    return ~max;
+}
+
